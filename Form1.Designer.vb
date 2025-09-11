e270957159c0472ffab16c6cc4220c2a6fe34c72
45mauro455 <mauro.omaraquino16@gmail.com>
2025-09-10 22:05:23 -0300

    se inserto un texbox al formulario

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -468,7 +468,7 @@ Partial Class Form1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(278, 23)
+        Me.TextBox2.Location = New System.Drawing.Point(359, 19)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(100, 20)
         Me.TextBox2.TabIndex = 17
